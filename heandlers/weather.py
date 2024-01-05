3e9f1c022cb7bbb7f804f0d6c87923da693a1013
YuriiSmakota <yurii.smakota@gmail.com>
2024-01-05 20:16:58 +0200

    add_description

--- heandlers/weather.py
+++ heandlers/weather.py
@@ -2,7 +2,7 @@ from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters.command import Command
 from aiogram.fsm.context import FSMContext
-from parsers.request_weather import get_temp
+from parsers.request_weather import RequestWeather
 from heandlers.city_input import InputCity
 
 router = Router()
@@ -19,5 +19,5 @@ async def command_weather(message: Message, state: FSMContext):
 async def city_input(message: Message):
 
     city = message.text
-    await message.answer(get_temp(city))
+    await message.answer(RequestWeather.get_weather(city))
 
--- parsers/request_weather.py
+++ parsers/request_weather.py
@@ -2,15 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_temp(city):
-    url = f"https://sinoptik.ua/погода-{city}"
-    try:
-        response = requests.get(url)
-        bs = BeautifulSoup(response.text, "lxml")
-        temp = bs.find("p", class_="today-temp").text
-        return f"Температура в городе <b>{city}</b>: <b>{temp}</b>"
-    except AttributeError:
-        return f"<b>{city}</b> город не найден, попробуйте ввести иначе: "
-
+class RequestWeather:
+    @staticmethod
+    def get_weather(city):
+        url = f"https://sinoptik.ua/погода-{city}"
+        try:
+            response = requests.get(url)
+            bs = BeautifulSoup(response.text, "lxml")
+            temp = bs.find("p", class_="today-temp").text
+            description = bs.find("div", class_="description").text
+            return (f"{description}\n"
+                    f"Температура: <b>{temp}</b>")
+        except AttributeError:
+            return f"<b>{city}</b> город не найден, попробуйте ввести иначе: "
 
 
